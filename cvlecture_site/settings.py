f20a26f2f753b819aca8e0b3e56810b84b1b58e8
KangTaekyung <moa1520@gmail.com>
2019-07-19 02:46:17 +0900

    settings.py

--- cvlecture_site/settings.py
+++ cvlecture_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x4wp(_2rqz11k)v@^&th!2$spamsiuz=t=a=bjg70(l0@34sl5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'moa1520.pythonanywhere.com']
 
 
 # Application definition
